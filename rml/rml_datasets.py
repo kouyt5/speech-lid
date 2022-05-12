cd0aed06236712375103bf76edb73fdb28c91615
kouyt5 <1147893200@qq.com>
2022-05-12 16:26:14 +0800

    为RML新增mix up

--- rml/rml_datasets.py
+++ rml/rml_datasets.py
@@ -8,7 +8,9 @@ import torch
 
 
 class RML16Dataset(Dataset):
-    def __init__(self, data: dict = None, key_mapping: dict = None, aug:bool = False) -> None:
+    def __init__(
+        self, data: dict = None, key_mapping: dict = None, aug: bool = False
+    ) -> None:
         """_summary_
 
         Args:
@@ -26,27 +28,45 @@ class RML16Dataset(Dataset):
     def __getitem__(self, index):
         key, snr, data = self.data[index]
         data = torch.from_numpy(data)
-        data = (data - torch.mean(data))/(torch.std(data)+1e-9)
-        
+        data = (
+            data - torch.mean(data)
+        ) / (torch.std(data) + 1e-9)
+
         # data aug
         if self.aug:
-            data = 1e-2*torch.rand_like(data) + data  # 噪声注入
-            mask_len = random.randint(0,60)  #30
-            point = random.randint(0, data.size(1)-mask_len)
-            data[:, point:point+mask_len] = torch.randn((mask_len,))
+            data = 1e-2 * torch.rand_like(data) + data  # 噪声注入
+            mask_len = random.randint(0, 60)  # 30
+            point = random.randint(0, data.size(1) - mask_len)
+            data[:, point : point + mask_len] = torch.randn((2, mask_len))
+            # data[0, point : point + mask_len] = torch.randn((mask_len,))
+            # mask_len = random.randint(0, 60)  # 30
+            # point = random.randint(0, data.size(1) - mask_len)
+            # data[1, point : point + mask_len] = torch.randn((mask_len,))
         return key, snr, data
 
     def collate_fn(self, batch):
-        data = torch.cat([batch[i][2].unsqueeze(0) for i in range(len(batch))], dim=0)  # N * 2 * 128
-        key = torch.LongTensor([self.key_mapping[batch[i][0]] for i in range(len(batch))])  # N
-        snrs = torch.LongTensor([self.snr2index(batch[i][1]) for i in range(len(batch))])
-        
+        data = torch.cat(
+            [batch[i][2].unsqueeze(0) for i in range(len(batch))], dim=0
+        )  # N * 2 * 128
+        key = torch.LongTensor(
+            [self.key_mapping[batch[i][0]] for i in range(len(batch))]
+        )  # N
+        snrs = torch.LongTensor(
+            [self.snr2index(batch[i][1]) for i in range(len(batch))]
+        )
+
         index = torch.randperm(len(batch))
-        data2 = torch.cat([batch[index[i].item()][2].unsqueeze(0) for i in range(len(batch))], dim=0)  # N * 2 * 128
-        key2 = torch.LongTensor([self.key_mapping[batch[index[i].item()][0]] for i in range(len(batch))])  # N
-        snrs2 = torch.LongTensor([self.snr2index(batch[index[i].item()][1]) for i in range(len(batch))])
+        data2 = torch.cat(
+            [batch[index[i].item()][2].unsqueeze(0) for i in range(len(batch))], dim=0
+        )  # N * 2 * 128
+        key2 = torch.LongTensor(
+            [self.key_mapping[batch[index[i].item()][0]] for i in range(len(batch))]
+        )  # N
+        snrs2 = torch.LongTensor(
+            [self.snr2index(batch[index[i].item()][1]) for i in range(len(batch))]
+        )
         return data, key, snrs, data2, key2, snrs2
-    
+
     def snr2index(self, snr):
         return (snr + 20) // 2
 
@@ -133,9 +153,14 @@ if __name__ == "__main__":
         split_type="unseen",
     )
 
-    dataset = RML16Dataset(data_manager.get_data("train"), data_manager.get_key_mapping())
-    dataloader = DataLoader(dataset=dataset, batch_size=8, shuffle=True, collate_fn=dataset.collate_fn)
-    for data,i in enumerate(dataloader):
+    dataset = RML16Dataset(
+        data_manager.get_data("train"), data_manager.get_key_mapping()
+    )
+    dataloader = DataLoader(
+        dataset=dataset, batch_size=8, shuffle=True, collate_fn=dataset.collate_fn
+    )
+    for data, i in enumerate(dataloader):
         data
         import soundfile as sf
-        sf.write("rml.wav", data=i[0][0].numpy().T, samplerate=200*1000)
+
+        sf.write("rml.wav", data=i[0][0].numpy().T, samplerate=200 * 1000)
