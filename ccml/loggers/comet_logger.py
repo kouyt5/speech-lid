0e970459943629e9b063ecf00473588135a2af4b
kouyt5 <1147893200@qq.com>
2023-03-15 16:07:09 +0800

    comet集成

---
+++ ccml/loggers/comet_logger.py
@@ -0,0 +1,30 @@
+from typing import Any, Dict
+import logging
+import torch.nn as nn
+from comet_ml import Experiment
+
+from ccml.loggers.base_logger import BaseLogger
+
+
+class CometLogger(BaseLogger):
+    def __init__(
+        self,
+        api_key: str = None,
+        project: str = None,  # 项目名，例如asr
+        entity: str = None,  # 用户名 kouyt5
+        name: str = None,  # 实验的名字
+    ):
+        self.experiment = Experiment(
+            api_key=api_key,
+            project_name=project,
+            workspace=entity,
+        )
+        self.experiment.set_name(name)
+        self.step = 1
+
+    def get_checkpoint_by_name(self, name: str, path: str = None):
+        return None
+
+    def log(self, data: Dict[str, Any], commit:bool=False, *args, **kwargs):
+        self.experiment.log_metrics(data, step=self.step, *args, **kwargs)
+        self.step += 1
